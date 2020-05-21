932fbab2cd70a1f6fb29256ca2bb894f2f32c2fd
jspark311 <josh.lindsay@gmail.com>
2020-05-21 08:44:38 -0600

    IMU abstraction was terrible. This is far superior.

---
+++ src/TripleAxisPipe.cpp
@@ -0,0 +1,236 @@
+/*
+File:   TripleAxisPipe.cpp
+Author: J. Ian Lindsay
+Date:   2020.05.14
+
+Copyright 2016 Manuvr, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "TripleAxisPipe.h"
+
+
+#if defined (ARDUINO)
+  #include <Arduino.h>
+#else
+  // The platform needs to supply this function.
+  extern uint32_t millis();
+#endif
+
+
+
+/*******************************************************************************
+* Static class utility members
+*******************************************************************************/
+
+static const char* TripleAxisPipe::spatialSenseStr(SpatialSense s) {
+  switch (s) {
+    case SpatialSense::UNITLESS:   return "UNITLESS";
+    case SpatialSense::ACC:        return "ACC";
+    case SpatialSense::GYR:        return "GYR";
+    case SpatialSense::MAG:        return "MAG";
+    case SpatialSense::EULER_ANG:  return "EULER_ANG";
+    default:
+      break;
+  }
+  return "UNDEFINED";
+}
+
+
+
+/*******************************************************************************
+* TripleAxisFork
+*******************************************************************************/
+
+/*
+* Behavior: Pushes left first. Then right, regardless of failure on left.
+*
+* @return 0 on sucess on both sides of the fork, -1 on one failure, or -2 on two failures.
+*/
+int8_t TripleAxisFork::pushVector(SpatialSense s, Vector3f* data, Vector3f* error = nullptr) {
+  int8_t ret = -2;
+  if ((nullptr != _LEFT) && (0 == _LEFT->pushVector(s, data, error))) {
+    ret++;
+  }
+  if ((nullptr != _RIGHT) && (0 == _RIGHT->pushVector(s, data, error))) {
+    ret++;
+  }
+  return ret;
+};
+
+
+void TripleAxisFork::printPipe(StringBuilder* output, uint8_t stage, uint8_t verbosity) {
+  StringBuilder indent;
+  for (uint8_t i = 0; i < stage; i++) {    indent.concat("    ");    }
+  output->concatf("%s+-< 3AxisPipe: Fork >-------------------\n", (char*) indent.string());
+  indent.clear();
+  if (nullptr != _LEFT) {
+    _LEFT->printPipe(output, stage+1, verbosity);
+  }
+  if (nullptr != _RIGHT) {
+    _LEFT->printPipe(output, stage+1, verbosity);
+  }
+}
+
+
+
+/*******************************************************************************
+* TripleAxisConvention
+*******************************************************************************/
+
+/*
+* Behavior: Refreshes this instance's state and calls callback, if defined.
+* Marks the data as fresh if the callback is either absent, or returns nonzero.
+*
+* @return -2 on null NXT, or return code from downstream pushVector() fxn.
+*/
+int8_t TripleAxisConvention::pushVector(SpatialSense s, Vector3f* data, Vector3f* error) {
+  Vector3f stacked_dat;
+  Vector3f stacked_err;
+  float inversion = 1.0;
+  int8_t ret = -2;
+  if (nullptr != _NXT) {
+    ret++;
+    switch (_SRC_FMT) {
+      case GnomonType::RH_NEG_X:
+      case GnomonType::LH_POS_X:
+        inversion = -1.0;
+      case GnomonType::RH_POS_X:
+      case GnomonType::LH_NEG_X:
+      case GnomonType::UNDEFINED:  // Undefinition results in no transform.
+        stacked_dat(inversion * data->x, data->y, data->z);
+        if (nullptr != error) {
+          stacked_err(inversion * error->x, error->y, error->z);
+        }
+        break;
+      case GnomonType::RH_NEG_Y:
+      case GnomonType::LH_POS_Y:
+        inversion = -1.0;
+      case GnomonType::RH_POS_Y:
+      case GnomonType::LH_NEG_Y:
+        stacked_dat(inversion * data->y, data->z, data->x);
+        if (nullptr != error) {
+          stacked_err(inversion * error->y, error->z, error->x);
+        }
+        break;
+      case GnomonType::RH_NEG_Z:
+      case GnomonType::LH_POS_Z:
+        inversion = -1.0;
+      case GnomonType::RH_POS_Z:
+      case GnomonType::LH_NEG_Z:
+        stacked_dat(inversion * data->z, data->x, data->y);
+        if (nullptr != error) {
+          stacked_err(inversion * error->z, error->x, error->y);
+        }
+        break;
+    }
+    if (0 == _NXT->pushVector(s, &stacked_dat, (nullptr != error) ? &stacked_err : nullptr)) {
+      ret = 0;
+    }
+  }
+  return ret;
+}
+
+
+void TripleAxisConvention::printPipe(StringBuilder* output, uint8_t stage, uint8_t verbosity) {
+  StringBuilder indent;
+  for (uint8_t i = 0; i < stage; i++) {    indent.concat("    ");    }
+  output->concatf("%s+-< 3AxisPipe: Convention >-------------\n", (char*) indent.string());
+  indent.clear();
+  if (nullptr != _NXT) {
+    _NXT->printPipe(output, stage+1, verbosity);
+  }
+}
+
+
+
+/*******************************************************************************
+* TripleAxisTerminus
+*******************************************************************************/
+
+/*
+* Atomic accessor with freshness management and return indication.
+*
+* @return 0 on success with stale data.
+* @return 1 on success with fresh data.
+*/
+int8_t TripleAxisTerminus::getDataWithErr(Vector3f* d, Vector3f* e, uint32_t* c) {
+  int8_t ret = _fresh_data ? 1 : 0;
+  _fresh_data = false;
+  d->set(&_DATA);
+  if (nullptr != c) {
+    *c = _update_count;
+  }
+  if ((nullptr != e) && (_has_error)) {
+    e->set(&_ERR);
+  }
+  return ret;
+}
+
+
+
+/*
+* Behavior: If sense parameter matches the local class sense, refreshes
+*   this instance's state and calls callback, if defined. Marks the data
+*   as fresh if the callback is either absent, or returns nonzero.
+*
+* @return 0 on refresh, or -1 on sense mis-match.
+*/
+int8_t TripleAxisTerminus::pushVector(SpatialSense s, Vector3f* data, Vector3f* error) {
+  int8_t ret = -1;
+  if (_SENSE == s) {
+    _last_update = millis();
+    _update_count++;
+    _DATA.set(data);
+    if (nullptr != error) {
+      _ERR.set(error);
+      _has_error = true;
+    }
+    if (nullptr != _CALLBACK) {
+      _fresh_data = (0 != _CALLBACK(_SENSE, &_DATA, (_has_error ? &_ERR : nullptr), _update_count));
+    }
+    else {
+      _fresh_data = true;
+    }
+  }
+  return ret;
+}
+
+
+/*
+* Resets the class to zero.
+*/
+void TripleAxisTerminus::reset() {
+  _has_error    = false;
+  _fresh_data   = false;
+  _last_update  = 0;
+  _update_count = 0;
+  _DATA.set(0.0, 0.0, 0.0);
+  _ERR.set(0.0, 0.0, 0.0);
+}
+
+
+void TripleAxisTerminus::printPipe(StringBuilder* output, uint8_t stage, uint8_t verbosity) {
+  StringBuilder indent;
+  for (uint8_t i = 0; i < stage; i++) {    indent.concat("    ");    }
+  output->concatf("%s+-< 3AxisPipe: Terminus >---------------\n", (char*) indent.string());
+  output->concatf("%s| Has callback:   %c\n", (char*) indent.string(), (nullptr != _CALLBACK)?'y':'n');
+  output->concatf("%s| Seq number:     %u\n", (char*) indent.string(), _update_count);
+  output->concatf("%s| SpatialSense:   %s\n", (char*) indent.string(), TripleAxisPipe::spatialSenseStr(_SENSE));
+  output->concatf("%s| Value %s:    (%.3f, %.3f, %.3f)\n", (char*) indent.string(), (_fresh_data?"FRESH":"STALE"), _DATA.x, _DATA.y, _DATA.z);
+  if (_has_error) {
+    output->concatf("%s| Error:          (%.3f, %.3f, %.3f)\n", (char*) indent.string(), _ERR.x, _ERR.y, _ERR.z);
+  }
+  output->concatf("%s| Last update:    %u\n", (char*) indent.string(), _last_update);
+}
---
+++ src/TripleAxisPipe.h
@@ -0,0 +1,255 @@
+/*
+File:   TripleAxisPipe.h
+Author: J. Ian Lindsay
+Date:   2020.05.14
+
+Copyright 2016 Manuvr, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <inttypes.h>
+#include <stdint.h>
+#include "Vector3.h"
+#include "StringBuilder.h"
+
+#ifndef __TRIPLE_AXIS_PIPE_H__
+#define __TRIPLE_AXIS_PIPE_H__
+
+
+/*******************************************************************************
+* Types and interface
+*******************************************************************************/
+
+/* Different 3-axis sensors this interface supports. */
+enum class SpatialSense : uint8_t {
+  UNITLESS = 0, // Unitless scalar
+  ACC,          // Accelerometer. Data/error is given in m/s^2.
+  GYR,          // Gyroscope. Data/error is given in rad/s.
+  MAG,          // Magnetometer. Data/error is given in Teslas.
+  EULER_ANG     // Orientation (Roll, Pitch, Yaw). Data/error is given in radians.
+  // TODO: Consider adding statistical and arithmetic intermediary results? EG, STDEV/RMS/MEAN/Etc...
+};
+
+
+/* Shorthand for a pointer to a callback for value updates.  */
+typedef int8_t (*TripleAxisTerminalCB)(SpatialSense, Vector3f* dat, Vector3f* err, uint32_t seq_num);
+
+
+/*
+* Pure virtual class to define a uniform interface between sensors that produce
+*   and process 3-axis data.
+*/
+class TripleAxisPipe {
+  public:
+    /*
+    * NOTE: This API only supports symetrical error bars. Might need to extend it.
+    *
+    * @return 0 on success. Less-than zero otherwise.
+    */
+    virtual int8_t pushVector(SpatialSense, Vector3f* data, Vector3f* error = nullptr) =0;
+    virtual void   printPipe(StringBuilder*, uint8_t stage, uint8_t verbosity) =0;
+
+    static const char* spatialSenseStr(SpatialSense);
+};
+
+
+
+/*******************************************************************************
+* Instantiable utility classes
+*******************************************************************************/
+
+/**
+* An instantiable TripleAxisPipe that forks a single afferent into two
+*   efferents. Retains no state apart from refs to its two efferent pathways,
+*   denoted "left" and "right".
+*/
+class TripleAxisFork : public TripleAxisPipe {
+  public:
+    TripleAxisFork() {};
+    TripleAxisFork(TripleAxisPipe* l, TripleAxisPipe* r) : _LEFT(l), _RIGHT(r) {};
+    ~TripleAxisFork() {};
+
+    inline void setLeft(TripleAxisPipe* l) {   _LEFT  = l;  };
+    inline void setRight(TripleAxisPipe* r) {  _RIGHT = r;  };
+
+    /**
+    * Behavior: Pushes left first. Then right, regardless of failure on left.
+    *
+    * @return 0 on sucess on both sides of the fork, -1 on one failure, or -2 on two failures.
+    */
+    int8_t pushVector(SpatialSense s, Vector3f* data, Vector3f* error = nullptr);
+    void   printPipe(StringBuilder*, uint8_t stage, uint8_t verbosity);
+
+
+  private:
+    TripleAxisPipe* _LEFT  = nullptr;
+    TripleAxisPipe* _RIGHT = nullptr;
+};
+
+
+
+/**
+* An instantiable TripleAxisPipe that performs a coordinate transform on
+*   afferent data to convert it into the conventions of aircraft principal axes
+*   before forwarding it onward. Does not mutate the provided data.
+*
+* NOTE: This class should probably be the first stage after the source in any
+*   pipeline where all of the following conditions are true of the afferent data:
+*   1) Native axis arrangement is important to know (IE, a compass).
+*   2) Native axis arrangement does not match the desired arrangement.
+*   3) Native axis arrangement differs from any other 3-axis data with which it might be muxed.
+*/
+class TripleAxisConvention : public TripleAxisPipe {
+  public:
+    TripleAxisConvention() {};
+    TripleAxisConvention(TripleAxisPipe* nxt, GnomonType g) : _NXT(nxt), _SRC_FMT(g) {};
+    ~TripleAxisConvention() {};
+
+    inline void setNext(TripleAxisPipe* n) {        _NXT  = n;      };
+    inline void setAfferentGnomon(GnomonType n) {   _SRC_FMT  = n;  };
+
+    /**
+    * Behavior: Refreshes this instance's state and calls callback, if defined.
+    * Marks the data as fresh if the callback is either absent, or returns nonzero.
+    *
+    * @return -2 on null NXT, or return code from downstream pushVector() fxn.
+    */
+    int8_t pushVector(SpatialSense s, Vector3f* data, Vector3f* error = nullptr);
+    void   printPipe(StringBuilder*, uint8_t stage, uint8_t verbosity);
+
+
+  private:
+    TripleAxisPipe* _NXT = nullptr;
+    GnomonType      _SRC_FMT = GnomonType::RH_POS_Z;  // Defaults to no operation.
+};
+
+
+
+/**
+* Latin: Single end
+*
+* An instantiable TripleAxisPipe that functions as a storage and change-notice
+*   sink at the end of a pipeline, and which handles the single SpatialSense
+*   type that the class was constructed with.
+*/
+class TripleAxisTerminus : public TripleAxisPipe {
+  public:
+    TripleAxisTerminus(const SpatialSense s, const TripleAxisTerminalCB cb) : _CALLBACK(cb), _SENSE(s) {};
+    TripleAxisTerminus(const SpatialSense s) : TripleAxisTerminus(s, nullptr) {};   // For instances that will be polled.
+    ~TripleAxisTerminus() {};
+
+    inline const SpatialSense sense() {    return _SENSE;           };
+    inline Vector3f*    getData() {        return &_DATA;           };
+    inline Vector3f*    getError() {       return &_ERR;            };
+    inline uint32_t     lastUpdate() {     return _last_update;     };
+    inline uint32_t     updateCount() {    return _update_count;    };
+    inline bool         haveError() {      return _has_error;       };
+    inline bool         dataFresh() {      return _fresh_data;      };
+
+    /**
+    * Atomic accessor with freshness management and return.
+    *
+    * @return 0 on success with stale data.
+    * @return 1 on success with fresh data.
+    */
+    int8_t getDataWithErr(Vector3f* d, Vector3f* e, uint32_t* seq_num);
+
+    /**
+    * Behavior: If sense parameter matches the local class sense, refreshes
+    *   this instance's state and calls callback, if defined. Marks the data
+    *   as fresh if the callback is either absent, or returns nonzero.
+    *
+    * @return 0 on success, or -1 on sense mis-match.
+    */
+    int8_t pushVector(SpatialSense s, Vector3f* data, Vector3f* error = nullptr);
+    void   printPipe(StringBuilder*, uint8_t stage, uint8_t verbosity);
+
+    /**
+    * Resets the class to zero.
+    */
+    void reset();
+
+
+  private:
+    const TripleAxisTerminalCB _CALLBACK;
+    const SpatialSense         _SENSE;
+    bool     _has_error    = false;   // TODO: Move bools below into an integer field for alignment authority.
+    bool     _fresh_data   = false;   // TODO: Move bools below into an integer field for alignment authority.
+    uint16_t _PADDING      = 0;
+    uint32_t _last_update  = 0;
+    uint32_t _update_count = 0;
+    Vector3f _DATA;
+    Vector3f _ERR;
+};
+
+
+/**
+* TODO:
+* Latin: All ends
+* An instantiable TripleAxisPipe that functions as a storage and change-notice
+*   sink at the end of a pipeline, and which handles all SpatialSense types it
+*   receives.
+*
+* NOTE: Requires dynamic memory, but might be a lower-overhead choice versus
+*   three independent copies of TripleAxisTerminus to represent a 9-DoF sensor.
+*/
+//class TripleAxisOmnitermini : public TripleAxisPipe {
+//};
+
+
+/**
+* TODO:
+* An instantiable TripleAxisPipe that filters data from a single sense.
+* By default, this class will ignore (and decline to relay) all non-matching
+*   senses. But this behavior can be changed to ignore-with-relay.
+* By default, this class absorbs matching afferent data and re-emits the
+*   filter's output as its efferent with the same SpatialSense. But this
+*   behavior can be changed to relay the afferent data, and enrich the pipeline
+*   with a novel SpatialSense, as an orientation filter would.
+* The filtering functionality is provided by SensorFilter3.
+*/
+//class TripleAxisSingleFilter : public TripleAxisPipe {
+//};
+
+
+/**
+* TODO:
+* An instantiable TripleAxisPipe that filters data from many senses.
+* By default, this class will ignore (and decline to relay) all non-matching
+*   senses. But this behavior can be changed to ignore-with-relay.
+* By default, this class absorbs matching afferent data and re-emits the
+*   filter's output as its efferent with the same SpatialSense. But this
+*   behavior can be changed to relay the afferent data, and enrich the pipeline
+*   with a novel SpatialSense, as an orientation filter would.
+* The filtering functionality is provided by SensorFilter3.
+*/
+//class TripleAxisMultiFilter : public TripleAxisPipe {
+//};
+
+
+/**
+* TODO:
+* An instantiable TripleAxisPipe takes MAG/ACC/GYR, and produces EULER_ANG as
+*   its efferent.
+* By default, this class relays matching afferent data and inhibits non-match
+*   relay. But this behavior can be changed.
+* TODO: Internally, this class should use a quat to eliminate gimble lock and
+*   reduce branching. But efferent data should be converted to Euler angles for
+*   insertion into the pipeline.
+*/
+//class TripleAxisOrientation : public TripleAxisPipe {
+//};
+
+
+#endif // __TRIPLE_AXIS_PIPE_H__
