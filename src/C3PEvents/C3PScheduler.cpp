8b1487dd7c587d0b5a939c8a42e1f59b84067aa7
jspark311 <josh.lindsay@gmail.com>
2023-05-15 23:45:56 -0600

    Initial (broken) version of C3PScheduler.

---
+++ src/C3PEvents/C3PScheduler.cpp
@@ -0,0 +1,201 @@
+/*
+File:   C3PScheduler.cpp
+Author: J. Ian Lindsay
+Date:   2013.07.10
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "C3PScheduler.h"
+
+
+// Singleton instance
+static C3PScheduler* SCHEDULER_INSTANCE_PTR = nullptr;
+
+C3PScheduler* C3PScheduler::getInstance() {
+  if (nullptr == SCHEDULER_INSTANCE_PTR) {
+    SCHEDULER_INSTANCE_PTR = new C3PScheduler(8);  // TODO: Arbitrary schedule limit.
+    SCHEDULER_INSTANCE_PTR->_active.allocated();
+    SCHEDULER_INSTANCE_PTR->_exec_queue.allocated();
+  }
+  return SCHEDULER_INSTANCE_PTR;
+}
+
+
+/*******************************************************************************
+* C3PSchedule base class
+*******************************************************************************/
+
+int8_t C3PSchedule::execute() {
+  _executing = true;
+  profiler.markStart();
+  int8_t ret = _execute();
+  if (willRunAgain()) {
+    _recurrances--;
+  }
+  else {
+    _enabled = false;
+    _exec_at = 0;
+  }
+  profiler.markStop();
+  _executing = false;
+  return ret;
+}
+
+
+void C3PSchedule::delay(uint32_t by_us) {
+  if (!_executing) {
+    if (!_enabled) {
+      _exec_at = micros();
+      _enabled = true;
+    }
+    _exec_at += by_us;
+  }
+}
+
+
+void C3PSchedule::delay() {
+  if (!_executing) {
+    _exec_at = (micros() + _period);
+    _enabled = true;
+  }
+}
+
+
+bool C3PSchedule::willRunAgain() {
+  bool ret = (_enabled & (0 < _recurrances));
+  return ret;
+}
+
+
+void C3PSchedule::printSchedule(StringBuilder* output) {
+  _print_schedule(output);
+  output->concatf("\tPeriod:          %u\n", _period);
+  output->concatf("\tRecurrances:     %d\n", _recurrances);
+  if (willRunAgain()) {
+    output->concatf("\tNext execution:  %u (%uus from now)\n", _exec_at, wrap_accounted_delta(_exec_at, micros()));
+  }
+  if (0 > profiler.executions()) {
+    output->concatf("\tLast execution:  %u (%uus ago)\n", _last_exec, wrap_accounted_delta(micros(), _last_exec));
+  }
+  StopWatch::printDebugHeader(output);
+  profiler.printDebug("execute()", output);
+}
+
+
+
+/*******************************************************************************
+* Kinds of Schedules
+*******************************************************************************/
+
+int8_t C3PScheduledPolling::_execute() {
+  int8_t ret = 0;
+  _pollable_obj->poll();
+  return ret;
+}
+
+void C3PScheduledPolling::_print_schedule(StringBuilder* output) {
+  StringBuilder::styleHeader1(output, "ScheduledPolling");
+}
+
+
+
+int8_t C3PScheduledLambda::_execute() {
+  int8_t ret = 0;
+  _fxn_lambda();
+  return ret;
+}
+
+void C3PScheduledLambda::_print_schedule(StringBuilder* output) {
+  StringBuilder::styleHeader1(output, "ScheduledLambda");
+}
+
+
+
+/*******************************************************************************
+* The Scheduler singleton
+*******************************************************************************/
+
+/*
+* Destructor
+*/
+C3PScheduler::~C3PScheduler() {
+  // TODO: Wipe the queues. Block until no executions are happening. Might not
+  //   be important for the singleton pattern.
+}
+
+
+/**
+* Adds a Schedule to our processing queue.
+*
+* @param sch The Schedule to add.
+* @return 0 on success, or negative on error.
+*/
+int8_t C3PScheduler::addSchedule(C3PSchedule* sch) {
+  int8_t ret = _active.insertIfAbsent(sch);
+  if (0 == ret) {
+    if (sch->enabled()) {
+      sch->delay();  // Will mark enabled,
+    }
+  }
+  return ret;
+}
+
+
+int8_t C3PScheduler::removeSchedule(C3PSchedule* sch) {
+  int8_t ret = -1;
+  // TODO
+  return ret;
+}
+
+
+void C3PScheduler::serviceScheduledEvents() {
+  if (_isr_count > 0) {
+    profiler_deadband.markStop();
+    profiler_service.markStart();
+    // Service queue will refuse to process until the ISR has been pinged.
+    C3PSchedule* current = _exec_queue.get();
+    while (nullptr != current) {
+      current->execute();
+      current = _exec_queue.get();
+    }
+    profiler_service.markStop();
+  }
+}
+
+
+void C3PScheduler::advanceScheduler() {
+  for (uint32_t i = 0; i < _active.count(); i++) {
+    C3PSchedule* current = _active.get(i, false);
+    if ((nullptr != current) && current->enabled()) {
+
+      if (current->_exec_at <= micros()) {   // TODO: Wrap handling.
+        if (0 != _exec_queue.insertIfAbsent(current)) {
+          // TODO: Anomaly tracking...
+        }
+      }
+    }
+  }
+  profiler_deadband.markStart();
+  _isr_count++;
+}
+
+
+void C3PScheduler::printDebug(StringBuilder* output) {
+  StringBuilder::styleHeader1(output, "C3PScheduler");
+  output->concatf("\tSchedule count:   %u\n", _active.count());
+  output->concatf("\tLoops (SVC/ISR):  %u / %u\n\n", profiler_service.executions(), _isr_count);
+  StopWatch::printDebugHeader(output);
+  profiler_service.printDebug("Service", output);
+  profiler_deadband.printDebug("Deadband", output);
+}
---
+++ src/C3PEvents/C3PScheduler.h
@@ -0,0 +1,204 @@
+/*
+File:   C3PScheduler.h
+Author: J. Ian Lindsay
+Date:   2013.07.10
+
+This class is meant to be an idle-time task scheduler for small microcontrollers. It
+should be driven by a periodic interrupt of some sort, but it may also be effectively
+used with a reliable polling scheme (at the possible cost of timing accuracy).
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef __C3P_SCHEDULER_H
+#define __C3P_SCHEDULER_H
+
+#include <functional>
+#include "../StringBuilder.h"
+#include "../CppPotpourri.h"
+#include "../FlagContainer.h"
+#include "../StopWatch.h"
+#include "../PriorityQueue.h"
+#include "../ElementPool.h"
+#include "../AbstractPlatform.h"
+#include "../C3PLogger.h"
+
+
+/*******************************************************************************
+* This pure-virtual class represents a single schedule. The behavior of Schedule
+*   execution should be confined to extending classes that intend on being
+*   processed (possibly in an ISR stack frame) by the Scheduler singleton.
+*
+* NOTE: If (_recurrances < 0), then the schedule recurs for as long as
+*   it remains enabled. If the value is zero, the schedule is disabled upon
+*   successful execution. If the value is anything else, the schedule remains
+*   enabled and this value is decremented.
+*
+* TODO: There is no implication of heap churn anywhere in this class (which may
+*   be preferrable). But complicates things in other ways. This might end up
+*   being ref-counted. But until then, Scheduler does no memory management.
+*******************************************************************************/
+class C3PSchedule {
+  public:
+    StopWatch  profiler;                  // If this schedule is being profiled, the ref will be here.
+
+    virtual ~C3PSchedule() {};
+
+    //inline int8_t executeNow() {       return -1;   };
+    inline bool     enabled() {               return _enabled;      };
+    inline void     enabled(bool x) {         _enabled = x;         };
+    inline int32_t  recurrence() {            return _recurrances;  };
+    inline void     recurrence(int32_t x) {   _recurrances = x;     };
+    inline uint32_t period() {                return _period;       };
+    inline void     period(uint32_t x) {      _period = x;          };
+
+    int8_t execute();
+    void delay(uint32_t by_us);  // Set the schedule's TTW to the given value this execution only.
+    void delay();                // Reset the given schedule to its period and enable it.
+    bool willRunAgain();         // Returns true if the indicated schedule will fire again.
+    void printSchedule(StringBuilder*);
+
+
+  protected:
+    C3PSchedule(const uint32_t PERIOD, const int32_t RECURANCES, const bool ENABLED) :
+      _last_exec(0),
+      _exec_at(0),
+      _period(PERIOD),
+      _recurrances(RECURANCES),
+      _enabled(ENABLED),
+      _executing(false) {};
+
+    /* Classes that wish to be considered as Schedules must implement these. */
+    virtual int8_t _execute() =0;
+    virtual void _print_schedule(StringBuilder*) =0;
+
+
+  private:
+    friend class C3PScheduler;  // Scheduler itself is allowed access to all members.
+    uint32_t   _last_exec;      // At what time was the last execution?
+    uint32_t   _exec_at;        // At what time will be the next execution?
+    uint32_t   _period;         // How often does this schedule execute?
+    int32_t    _recurrances;    // How many times will execution occur? See notes.
+    bool       _enabled;        // If true, this schedule will be processed.
+    bool       _executing;      // If true, this schedule is presently executing.
+    //bool       _run_in_isr;     // If true, this schedule will be processed in the ISR stack frame. Very dangerous.
+    //bool       _autoclear;      // If true, this schedule will be removed after its last execution.
+};
+
+
+/*******************************************************************************
+* Kinds of Schedules
+*******************************************************************************/
+/**
+* A concrete type of Schedule that calls the given object's poll() function.
+* This is mostly for fitting simpler classes into a Schedule-driven application.
+*/
+class C3PScheduledPolling : public C3PSchedule {
+  public:
+    C3PScheduledPolling(const uint32_t PERIOD, const int32_t RECURANCES, const bool ENABLED, PollableObj* obj) :
+      C3PSchedule(PERIOD, RECURANCES, ENABLED), _pollable_obj(obj) {};
+
+    virtual ~C3PScheduledPolling() {};
+
+
+  protected:
+    PollableObj* _pollable_obj;          // Pointer to the class to be polled.
+
+    /* Obligate overrides from C3PSchedule... */
+    int8_t _execute();
+    void _print_schedule(StringBuilder*);
+};
+
+
+/**
+* A concrete type of Schedule that calls the given lamba function on execution.
+*/
+class C3PScheduledLambda : public C3PSchedule {
+  public:
+    C3PScheduledLambda(const uint32_t PERIOD, const int32_t RECURANCES, const bool ENABLED, std::function<int8_t(void)> lam) :
+      C3PSchedule(PERIOD, RECURANCES, ENABLED), _fxn_lambda(lam) {};
+
+    virtual ~C3PScheduledLambda() {};
+
+
+  protected:
+    std::function<int8_t(void)> _fxn_lambda;  // A lambda to be called.
+
+    /* Obligate overrides from C3PSchedule... */
+    int8_t _execute();
+    void _print_schedule(StringBuilder*);
+};
+
+
+/**
+* A concrete type of Schedule that shows the given Event to the given Observer.
+*/
+//class C3PScheduledEvent : public C3PSchedule {
+//  public:
+//    C3PScheduledEvent(const uint32_t PERIOD, const int32_t RECURANCES, const bool ENABLED, C3PEvent* evnt, C3PObserver* obs) :
+//      C3PSchedule(PERIOD, RECURANCES, ENABLED), _event(evnt), _observer(obs) {};
+//
+//    virtual ~C3PScheduledEvent() {};
+//
+//
+//  protected:
+//    C3PEvent*    _event;
+//    C3PObserver* _observer;
+//
+//    /* Obligate overrides from C3PSchedule... */
+//    int8_t _execute();
+//    void _print_schedule(StringBuilder*);
+//};
+
+
+
+/*******************************************************************************
+* The Scheduler singleton
+*
+* NOTE: All times are given as microseconds, and are 32-bit. That makes the
+*   maximum useful schedule period a bit more than 71.5 minutes.
+*******************************************************************************/
+class C3PScheduler {
+  public:
+    StopWatch  profiler_service;   // Number of calls to serviceScheduledEvents() that actually ran a schedule.
+    StopWatch  profiler_deadband;  // Used to make inferences about jitter.
+
+    ~C3PScheduler();  // Destructor
+
+    int8_t addSchedule(C3PSchedule*);
+    int8_t removeSchedule(C3PSchedule*);
+    void serviceScheduledEvents();        // Execute any schedules that have come due.
+    void advanceScheduler();              // Push all enabled schedules forward by one tick.
+    void printDebug(StringBuilder*);
+
+    static C3PScheduler* getInstance();   // Enforcement of singleton pattern.
+
+
+  private:
+    // TODO: No memory management strategy forces some waste in RingBuffer...
+    RingBuffer<C3PSchedule*> _active;
+    RingBuffer<C3PSchedule*> _exec_queue;
+    //RingBuffer<C3PSchedule*> _inactive;
+    uint32_t _isr_count;
+
+    /* Constructor */
+    C3PScheduler(const uint32_t MAX_SCHEDULE_COUNT) :
+      _active(MAX_SCHEDULE_COUNT),
+      //_inactive(MAX_SCHEDULE_COUNT),
+      _exec_queue(MAX_SCHEDULE_COUNT),
+      _isr_count(0)
+    {};
+};
+
+#endif  // __C3P_SCHEDULER_H
