fbac32f259dccc9546ddeca78c8385c6f301ebf2
jspark311 <josh.lindsay@gmail.com>
2023-09-06 03:19:20 -0600

    Test structures settled on for LineCoDec. Should be mostly framed. Now to start grinding it out.

--- extras/unit_tests/LineTermCoDecTests.cpp
+++ extras/unit_tests/LineTermCoDecTests.cpp
@@ -26,24 +26,247 @@ This program tests LineCoDec.
 #include "BufferAccepter/TestFixtures/CoDecTestFixtures.h"
 
 /*******************************************************************************
+* Known-answers for line-termination codec tests.
+*
+* This will be a highly-permuted series of tests to assure proper line
+*   termination behaivors under a variety of simulated constraints.
+*******************************************************************************/
+// NOTE: We will permute each string with line-terminators added in the test
+//   procedure, and adjusting the expected results accordingly. But we don't
+//   want to re-code double the tests.
+const char* const KAT_INPUT_0 = "This string\nhas all\r\nthe possible\n\rline ending\rsequences.\nBut it\ndoesn't have\r\na terminator\n\rsequence at\rthe ending.";
+const char* const KAT_INPUT_1 = "This string\ronly has\rCR endings,\rand also no\rfinal terminator.";
+const char* const KAT_INPUT_2 = "This string\nonly has\nLF endings,\nand also no\nfinal terminator.";
+const char* const KAT_INPUT_3 = "This string\r\nonly has\r\nCRLF endings,\r\nand also no\r\nfinal terminator.";
+
+const int KAT_LEN_0 = strlen(KAT_INPUT_0);  // 116
+const int KAT_LEN_1 = strlen(KAT_INPUT_1);  // 62
+const int KAT_LEN_2 = strlen(KAT_INPUT_2);  // 62
+const int KAT_LEN_3 = strlen(KAT_INPUT_3);  // 68
+
+// NOTE: We will then permute each KAT under three basic conditions:
+//   1) No trickle or back-pressure.
+//   1) Trickle from source.
+//   1) Back-pressure from efferant.
+// NOTE: This value gives enough leeway for 100 CRLF inflations before it no
+//   longer becomes "wide open" for the sake of a single push.
+const int DRY_SOURCE_LIMIT   = 50;   // TODO: Fine-tuning parameters. Values arbitrary, but must be fixed and within undocumented bounds.
+const int FLOODED_SINK_LIMIT = 67;   // TODO: Fine-tuning parameters. Values arbitrary, but must be fixed and within undocumented bounds.
+const int WIDE_OPEN_CHANNEL  = (strict_max( strict_max(KAT_LEN_0, KAT_LEN_1), strict_max(KAT_LEN_2, KAT_LEN_3)) + 100);
+// TODO: I ^just... wow...
+//   Recode in preprocessor macro logic, as that would be preferable to
+//   this (multiply-nested ternarys that happen at runtime).
+
+// TODO: Doing these repeat special cases up here, because they would be
+//   unreadable inline.
+const int KAT_CB_NUL_XFORM_DS_0 = ((KAT_LEN_0 / DRY_SOURCE_LIMIT) + 1) + ((0 < KAT_LEN_0 % DRY_SOURCE_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_DS_1 = ((KAT_LEN_1 / DRY_SOURCE_LIMIT) + 1) + ((0 < KAT_LEN_1 % DRY_SOURCE_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_DS_2 = ((KAT_LEN_2 / DRY_SOURCE_LIMIT) + 1) + ((0 < KAT_LEN_2 % DRY_SOURCE_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_DS_3 = ((KAT_LEN_3 / DRY_SOURCE_LIMIT) + 1) + ((0 < KAT_LEN_3 % DRY_SOURCE_LIMIT) ? 1 : 0);
+
+const int KAT_CB_NUL_XFORM_FS_0 = ((KAT_LEN_0 / FLOODED_SINK_LIMIT) + 1) + ((0 < KAT_LEN_0 % FLOODED_SINK_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_FS_1 = ((KAT_LEN_1 / FLOODED_SINK_LIMIT) + 1) + ((0 < KAT_LEN_1 % FLOODED_SINK_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_FS_2 = ((KAT_LEN_2 / FLOODED_SINK_LIMIT) + 1) + ((0 < KAT_LEN_2 % FLOODED_SINK_LIMIT) ? 1 : 0);
+const int KAT_CB_NUL_XFORM_FS_3 = ((KAT_LEN_3 / FLOODED_SINK_LIMIT) + 1) + ((0 < KAT_LEN_3 % FLOODED_SINK_LIMIT) ? 1 : 0);
+
+/*
+* Each known-answer test is run under a variety of simulated constraints.
+* We permute on the boolean options for call-break, and consolidate the outcome
+*   into the three members indicating the correct number of breaks in each mode.
+*/
+struct lineterm_test_conditions {
+  const int push_chunk_length;   // How fragmented should the pushes be?
+  const int sink_buffer_limit;   // How constricted should the buffer sink be?
+  const int call_breaks_mode_0;  // How many call-breaks in this mode?
+  const int call_breaks_mode_1;  // How many call-breaks in this mode?
+  const int call_breaks_mode_2;  // How many call-breaks in this mode?
+};
+
+/* A one-off struct to hold test cases for the base64 codec. */
+struct lineterm_test_case {
+  const char* const test_description;
+  const char* const input;                        // The string fed to the test.
+  const char* const output;                       // Expected output.
+  const LineTerm    output_terminator;            // Optional replacement term.
+  const LineTerm    replace_0;                    // Optional search term.
+  const LineTerm    replace_1;                    // Optional search term.
+  const LineTerm    replace_2;                    // Optional search term.
+  struct lineterm_test_conditions conditions[3];  // We permute the test under 3 constraint sets.
+};
+
+/*
+* This is the list and substance of the KATs.
+*
+* NOTE: These tests are themselves subject to mistakes, and behavior is
+*   highly contingent upon condition parameters.
+* NOTE: Each test case is permuted on three parameters to the class. n^4 is
+*   expensive, but three of those four factors are small and bounded by test
+*   structure. Still, every top-level KAT in the list below will produce 18 test
+*   cases to assure that the input and output strings match every time.
+* Additionally, much permutation is _not_ being done (exhaustive search term
+*   use, buffer constraints, etc).
+*/
+struct lineterm_test_case lineterm_test_cases[] = {
+  { "Null-transform",
+    KAT_INPUT_0,
+    KAT_INPUT_0,  // Output should match input.
+    LineTerm::ZEROBYTE, LineTerm::ZEROBYTE, LineTerm::ZEROBYTE, LineTerm::ZEROBYTE,
+    {
+      { WIDE_OPEN_CHANNEL, WIDE_OPEN_CHANNEL,  1, 1, 1 },  // With a wide-open channel and no replacements, expect a single call-break.
+      { DRY_SOURCE_LIMIT,  WIDE_OPEN_CHANNEL,  KAT_CB_NUL_XFORM_DS_0, KAT_CB_NUL_XFORM_DS_0, KAT_CB_NUL_XFORM_DS_0 },  // Expect as many calls as required to move the whole string.
+      { WIDE_OPEN_CHANNEL, FLOODED_SINK_LIMIT, KAT_CB_NUL_XFORM_FS_0, KAT_CB_NUL_XFORM_FS_0, KAT_CB_NUL_XFORM_FS_0 }   // Expect as many calls as required to move the whole string.
+    },
+  },
+  { "Transform (CR)->LF",
+    KAT_INPUT_0,
+    "This string\nhas all\n\nthe possible\n\nline ending\nsequences.\nBut it\ndoesn't have\n\na terminator\n\nsequence at\nthe ending.",
+    LineTerm::LF, LineTerm::CR, LineTerm::ZEROBYTE, LineTerm::ZEROBYTE,
+    {
+      { WIDE_OPEN_CHANNEL, WIDE_OPEN_CHANNEL,  1, 1, 6 },  // Search terms define the call breaks.
+      { DRY_SOURCE_LIMIT,  WIDE_OPEN_CHANNEL,  KAT_CB_NUL_XFORM_DS_0, 6, 6 },  // Expect as many calls as required to move the whole string.
+      { WIDE_OPEN_CHANNEL, FLOODED_SINK_LIMIT, KAT_CB_NUL_XFORM_FS_0, 6, 6 }   // Expect as many calls as required to move the whole string.
+    },
+  },
+  { "Transform (LF)->CR",
+    KAT_INPUT_0,
+    "This string\rhas all\r\rthe possible\r\rline ending\rsequences.\rBut it\rdoesn't have\r\ra terminator\r\rsequence at\rthe ending.",
+    LineTerm::CR, LineTerm::LF, LineTerm::ZEROBYTE, LineTerm::ZEROBYTE,
+    {
+      { WIDE_OPEN_CHANNEL, WIDE_OPEN_CHANNEL,  1, 1, 7 },  // Search terms define the call breaks.
+      { DRY_SOURCE_LIMIT,  WIDE_OPEN_CHANNEL,  KAT_CB_NUL_XFORM_DS_0, 7, 7 },  // Expect as many calls as required to move the whole string.
+      { WIDE_OPEN_CHANNEL, FLOODED_SINK_LIMIT, KAT_CB_NUL_XFORM_FS_0, 7, 7 }   // Expect as many calls as required to move the whole string.
+    },
+  },
+  { "Transform (CRLF)->LF",
+    KAT_INPUT_0,
+    "This string\nhas all\nthe possible\n\rline ending\rsequences.\nBut it\ndoesn't have\na terminator\n\rsequence at\rthe ending.",
+    LineTerm::LF, LineTerm::CRLF, LineTerm::ZEROBYTE, LineTerm::ZEROBYTE,
+    {
+      { WIDE_OPEN_CHANNEL, WIDE_OPEN_CHANNEL,  1, 1, 2 },  // Search terms define the call breaks.
+      { DRY_SOURCE_LIMIT,  WIDE_OPEN_CHANNEL,  KAT_CB_NUL_XFORM_DS_0, 2, 2 },  // Expect as many calls as required to move the whole string.
+      { WIDE_OPEN_CHANNEL, FLOODED_SINK_LIMIT, KAT_CB_NUL_XFORM_FS_0, 2, 2 }   // Expect as many calls as required to move the whole string.
+    },
+  }
+};
+
+
+
+/*******************************************************************************
 * Tests for line-termination codec
 *******************************************************************************/
 
 /*
-* A one-off struct to hold test cases for the base64 codec
+*
 */
-struct lineterm_test_case {
-  const char* const test_description;
-  const char* const input;
-  const uint8_t*    ouput_full;
-  const int         break_count;
-};
+int line_term_trivial_tests() {
+  int ret = -1;
+  printf("Running trivial tests...\n");
+  StringBuilder offering;
+  BufAcceptTestSink test_sink;
+  Base64Decoder decoder(&test_sink);
+  BufAcceptTestSource test_source(&decoder);
+  test_source.setProfiler(&test_sink.profiler);
+  test_sink.bufferLimit(1000000);  // Ensure the whole buffer goes out at once.
+  test_source.pushLimit(1000000);  // Ensure the whole buffer goes out at once.
+
+  printf("\tPushing a buffer through a null transform results in the same buffer... ");
+  printf("\tPass.\n\tThat buffer has the same break count... ");
+  printf("\tPass.\n\tThat buffer matches what was pushed... ");
+  printf("\tPass.\n\tLineTerm::INVALID as the replacement sequence results in a null transform... ");
+
+  if (0 != ret) {
+    printf(" Fail.\n");
+    StringBuilder log;
+    log.concatf("\nRemaining offering contents: (%u bytes)\n", offering.length());
+    offering.printDebug(&log);
+    log.concat("\n");
+    test_source.printDebug(&log);
+    test_sink.printDebug(&log);
+    printf("\n%s\n", (const char*) log.string());
+  }
+  return ret;
+}
+
+
+/*
+*
+*/
+int line_term_known_answer_tests() {
+  int ret = -1;
+  printf("Running known-answer tests...\n");
+  const int CASE_COUNT = (sizeof(lineterm_test_cases) / sizeof(lineterm_test_case));
+  int  case_idx        = 0;  // NOTE: Cloaked fourth-order loop. Tread carefully.
+  int  condition_idx   = 0;  // NOTE: Cloaked fourth-order loop. Tread carefully.
+  int  break_mode_idx  = 0;  // NOTE: Cloaked fourth-order loop. Tread carefully.
+  bool has_term_at_end = 0;  // NOTE: Cloaked fourth-order loop. Tread carefully.
+  bool test_failed  = false;
+  while ((case_idx < CASE_COUNT) & !test_failed) {
+    printf(
+      "\tBeginning case %d (%s), Conditions %d, break-mode %d, %sterm at tail).\n",
+      case_idx, lineterm_test_cases[case_idx].test_description, condition_idx, break_mode_idx, (has_term_at_end ? "" : "no ")
+    );
+    test_failed = true;
+    BufAcceptTestSink test_sink;
+    LineEndingCoDec line_breaker(&test_sink);
+    BufAcceptTestSource test_source(&line_breaker);
+    test_source.setProfiler(&test_sink.profiler);
+
+    test_source.pushLimit(lineterm_test_cases[case_idx].conditions[condition_idx].push_chunk_length);
+    test_sink.bufferLimit(lineterm_test_cases[case_idx].conditions[condition_idx].sink_buffer_limit);
+
+    StringBuilder offering(lineterm_test_cases[case_idx].input);
+    StringBuilder check_string((uint8_t*) lineterm_test_cases[case_idx].output, strlen(lineterm_test_cases[case_idx].output));
+
+    printf("\tPushing the buffer through the harness source indicates full claim... ");
+    if (1 == test_source.pushBuffer(&offering)) {
+      int polling_count = test_source.pollUntilStagnant();
+      printf("Pass.\n\tTest harness moved at least one chunk... ");
+      if (polling_count) {
+        //printf("Pass (ran %d times).\n\tSink received a result of the correct length (%d)... ", polling_count, check_string.length());
+        //if (test_sink.take_log.length() == check_string.length()) {
+          printf("Pass.\n\tThe sink received the correct content... ");
+          if (1 == check_string.cmpBinString(test_sink.take_log.string(), test_sink.take_log.length())) {
+            printf("Pass.\n\tTest case %d passes.\n", case_idx);
+            test_failed = false;
+          }
+        //}
+      }
+    }
+
+    if (test_failed) {
+      printf(" Fail.\n");
+      StringBuilder log;
+      test_source.printDebug(&log);
+      test_sink.printDebug(&log);
+      printf("\n%s\n", (const char*) log.string());
+    }
+
+    // NOTE: Cloaked fourth-order loop. Tread carefully.
+    if (has_term_at_end) {           // NOTE: Subtle reliance on post-increment behavior throughout.
+      has_term_at_end = false;       // This was the permutation permutation with pseudo-flushed buffers. Reset for next call-break mode.
+      if (2 == break_mode_idx++) {   // Re-run the test case with new call-break expectations.
+        break_mode_idx = 0;          // This was the final call-break permutation. Reset for next conditions.
+        if (2 == condition_idx++) {  // Re-run the test case under a new set of conditions.
+          condition_idx = 0;         // This was the final permutation of conditions. Reset for next case.
+          case_idx++;                // Move to the next case.
+        }
+      }
+    }
+    else {
+      // Re-run the test with a sought terminator at the
+      //   end (to force flushing behavior).
+      has_term_at_end = true;
+    }
+  }
+  return (test_failed ? -1 : 0);
+}
 
 
 
 void print_types_line_term_codec() {
   printf("\tLineEndingCoDec       %u\t%u\n", sizeof(Image),   alignof(Image));
 }
+
 
 /*******************************************************************************
 * The main function.
@@ -53,10 +276,14 @@ int c3p_line_codec_test_main() {
   const char* const MODULE_NAME = "LineEndingCoDec";
   printf("===< %s >=======================================\n", MODULE_NAME);
 
+  //if (0 == line_term_trivial_tests()) {
   if (true) {
-    ret = 0;
+    if (0 == line_term_known_answer_tests()) {
+      ret = 0;
+    }
+    else printTestFailure(MODULE_NAME, "Known-answer tests");
   }
-  else printTestFailure(MODULE_NAME, "LineEndingCoDec fails tests");
+  else printTestFailure(MODULE_NAME, "Trivial tests");
 
   return ret;
 }
--- src/BufferAccepter/LineCoDec/LineCoDec.h
+++ src/BufferAccepter/LineCoDec/LineCoDec.h
@@ -18,14 +18,98 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-A class to enforce conformity and grouping of line-endings.
+A class to enforce conformity and grouping of line-endings. It has two
+independent and orthogonal concerns, either of which are optional.
 
+
+Optional feature: terminal sequence transform
+--------------------------------------------------------------------------------
 This class is the gateway between definitions of what defines a "line" of text
-  for internal firmware versus any external system.
-This class can be used to signal the accumulation of text only until a
-   complete line is received.
+  for internal firmware versus any external system. It ought to handle all line
+  endings knowable by the firmware at build time. These are defined by the
+  LineTerm enum in EnumeratedTypeCodes.h. The underpinnings of this feature is
+  a bounded search-and-replace.
+
+Special semantics surrounding ZEROBYTE:
+ZEROBYTE is taken to be a C-string, and is used by this class as a "disregard"
+  value for the field it is used with.
+If sequences other than ZEROBYTE are specified for search, and the replacement
+  value is set to ZEROBYTE, the result will the input buffer tokenized by the
+  search sequences, those sequences will be removed, and the buffer dispatched
+  according to the
+
+Behavior of LineEndingCoDec::pushBuffer():
+  If required, a terminator replacement will be done to the extent that the
+  resulting transformed buffer can fit in the downstream space constraint. If
+  a multibyte sequence is specified for search, the last bytes of the buffer
+  will be rejected if they produce an unresolved match. So be careful with those
+  CRLF systems...
+
+
+Optional feature: call-breaking
+--------------------------------------------------------------------------------
+Definition of "call-break": A call to efferant->pushBuffer() that is
+  strategically made to coincide with line termination sequences at the ends of
+  the buffers pushed. Usually for the sake of offloading implementation burden
+  of tracking line breaks in what would otherwise be a chunky stream with no
+  assurances.
+
+This class can be used to signal the accumulation of text only if a
+  complete "line" is received from upstream.
+
+Call-time semantics of LineEndingCoDec::pushBuffer() are always
+  independent of anything being done by this class. That is: a call to
+  LineEndingCoDec::pushBuffer() will never be construed as being a "line".
+  If a caller intends a line-ending, it should pass buffers that have as their
+  last bytes a sequence that will be recognized by this class as a terminator.
+
+However: LineEndingCoDec's treatment of its calls to the efferent->pushBuffer()
+  can optionally be made to coincide with received (and possibly transformed)
+  termination sequences.
+
+This feature is controlled by two booleans, with their binomial expansion of
+  behavioral expectations given below:
+
+                               holdUntilBreak(false)  |  holdUntilBreak(true)
+                             +------------------------|------------------------
+isometricCallAndBreak(false) |    MODE 0 (default)    |        MODE 1
+isometricCallAndBreak(true)  |        MODE 2          |        MODE 2
+
+Behavior of LineEndingCoDec::pushBuffer():
+MODE 0: (efferent figures it out if it cares)
+  Buffers will be accepted, transformed (if necessary), and forwarded as allowed
+  by the downstream capacity on every call, with call-break semantics entirely
+  decoupled from buffer content.
+
+
+MODE 1: (efferent would like to be given several complete lines-at-a-time)
+  Buffers will be accepted, transformed (if necessary), and forwarded as allowed
+  by the downstream capacity on every call, with call-break semantics concerned
+  only with breaking on a final terminal sequence (and perhaps encapsulating many).
+
+Example: Given input buffer offered in a single call to pushBuffer()...
+  "This string \n will be call-broken \n only once \n but this will never be taken."
+
+...this output will come through on the next call...
+  "This string \n will be call-broken \n only once \n"
+
+...and the remainder of the string " but this will never be taken." will be left
+  unclaimed in the offered buffer. And if it is offered again without a line
+  terminator having subsequently been added, it will reject the buffer entirely.
+
+
+MODE 2: (efferent would like to be given a single line-at-a-time)
+  Buffers will be accepted, transformed (if necessary), and forwarded as allowed
+  by the downstream capacity on every call, with call-break semantics divided to
+  coincide directly with terminal sequences. That is, every received "line" will
+  be divided out into its own call to efferent->pushBuffer().
+  In the example given above, the same content would be delivered across three
+  call-breaks, and will leave exactly the same content as was left in MODE 1.
+
+
 
 Rules:
+--------------------------------------------------------------------------------
 1) hold_until_break will only permit passage of the buffer if it contains
     a break, and if so, only forwards the buffer up to (and including) the
     last break in the offered buffer.
@@ -33,7 +117,9 @@ Rules:
     of it). If set, the codec will chunk the inbound data by line-breaks,
     and will forward each to the downstream BufferAccepter, one at a time.
 3) Replacement is not assumed. With no replacement requested, this class will
-    simply chunk output using the specified LineTerm.
+    simply chunk output using the specified LineTerms (if any).
+
+
 */
 
 #include "../BufferAccepter.h"
@@ -45,7 +131,7 @@ Rules:
 
 /*
 */
-class LineEndingCoDec : public BufferAccepter {
+class LineEndingCoDec : public BufferCoDec {
   public:
     LineEndingCoDec(BufferAccepter* targ = nullptr, LineTerm t = LineTerm::ZEROBYTE) : _output_target(targ), _term_seq(t) {};
     ~LineEndingCoDec() {};
