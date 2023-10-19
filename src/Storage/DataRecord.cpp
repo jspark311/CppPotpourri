f56ef99adbe9696feab9d0f72eaca5b55a81e81d
jspark311 <josh.lindsay@gmail.com>
2023-10-18 22:27:31 -0600

    Adding async storage classes back in. They will be simplified later.

--- src/Storage/DataRecord.cpp
+++ src/Storage/DataRecord.cpp
@@ -1,5 +1,5 @@
 /*
-File:   Storage.cpp
+File:   DataRecord.cpp
 Author: J. Ian Lindsay
 Date:   2016.08.28
 
@@ -18,101 +18,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-This is the basal implementation of the storage interface.
+This is the basal implementation of a DataRecord to be used with Storage.
 */
 
 #include "Storage.h"
 #include "cbor-cpp/cbor.h"
 
 
-
-const char* DataRecord::recordTypeStr(uint8_t e) {
+const char* DataRecord::recordTypeStr(const StorageRecordType e) {
   switch (e) {
-    case STORAGE_RECORD_TYPE_UNINIT:       return "UNINITIALIZED";
-    case STORAGE_RECORD_TYPE_ROOT:         return "ROOT";
-    case STORAGE_RECORD_TYPE_KEY_LISTING:  return "KEY_LISTING";
-    case STORAGE_RECORD_TYPE_LOG:          return "LOG";
-    case STORAGE_RECORD_TYPE_USER_CONF:    return "USER_CONF";
-    case STORAGE_RECORD_TYPE_LOCATION:     return "LOCATION";
+    case StorageRecordType::UNINIT:           return "UNINIT";
+    case StorageRecordType::ROOT:             return "ROOT";
+    case StorageRecordType::KEY_LISTING:      return "KEY_LISTING";
+    case StorageRecordType::C3POBJ_ON_ICE:    return "C3POBJ_ON_ICE";
+    case StorageRecordType::LOG:              return "LOG";
+    case StorageRecordType::CONFIG_OBJ:       return "CONFIG_OBJ";
+    case StorageRecordType::FIRMWARE_BLOB:    return "FIRMWARE_BLOB";
+    case StorageRecordType::INVALID:          return "INVALID";
   }
   return "UNKNOWN";
 }
-
-const char* Storage::errStr(const StorageErr e) {
-  switch (e) {
-    case StorageErr::KEY_CLOBBERED:    return "KEY_CLOBBERED";
-    case StorageErr::NONE:             return "NONE";
-    case StorageErr::UNSPECIFIED:      return "UNSPECIFIED";
-    case StorageErr::BAD_PARAM:        return "BAD_PARAM";
-    case StorageErr::BUSY:             return "BUSY";
-    case StorageErr::MEM_ALLOC:        return "MEM_ALLOC";
-    case StorageErr::NOT_MOUNTED:      return "NOT_MOUNTED";
-    case StorageErr::NOT_READABLE:     return "NOT_READABLE";
-    case StorageErr::NOT_WRITABLE:     return "NOT_WRITABLE";
-    case StorageErr::NO_FREE_SPACE:    return "NO_FREE_SPACE";
-    case StorageErr::HW_FAULT:         return "HW_FAULT";
-    case StorageErr::KEY_NOT_FOUND:    return "KEY_NOT_FOUND";
-    case StorageErr::KEY_COLLISION:    return "KEY_COLLISION";
-  }
-  return "UNKNOWN";
-}
-
-/*******************************************************************************
-* Storage base-class functions
-*******************************************************************************/
-
-/**
-* Protected constructor
-*/
-Storage::Storage(const uint32_t ds_bytes, const uint32_t bs_bytes) :
-  DEV_SIZE_BYTES(ds_bytes),
-  DEV_BLOCK_SIZE(bs_bytes),
-  DEV_TOTAL_BLOCKS(ds_bytes / bs_bytes),
-  DEV_ADDR_SIZE_BYTES((ds_bytes < 65536) ? 2:4) {};
-
-
-void Storage::_print_storage(StringBuilder* output) {
-  output->concatf("-- Storage [%cr %cw, %sencrypted, %sremovable]\n",
-    _pl_flag(PL_FLAG_MEDIUM_READABLE) ? '+' : '-',
-    _pl_flag(PL_FLAG_MEDIUM_WRITABLE) ? '+' : '-',
-    _pl_flag(PL_FLAG_ENCRYPTED) ? "" : "un",
-    _pl_flag(PL_FLAG_REMOVABLE) ? "" : "non-"
-  );
-  output->concatf("-- %u total bytes across %u pages of %u bytes each.\n", DEV_SIZE_BYTES, DEV_TOTAL_BLOCKS, DEV_BLOCK_SIZE);
-  output->concatf("\t Address size:\t %u\n", DEV_ADDR_SIZE_BYTES);
-  if (isMounted()) {
-    output->concatf("\t Medium mounted (%u bytes free)\t%s\n",
-      freeSpace(),
-      isBusy() ? "[BUSY]" : ""
-    );
-  }
-  if (_pl_flag(PL_FLAG_USES_FILESYSTEM)) {
-    output->concat("\t On top of FS\n");
-  }
-  if (_pl_flag(PL_FLAG_BLOCK_ACCESS)) {
-    output->concat("\t Block access\n");
-  }
-  if (_pl_flag(PL_FLAG_BATTERY_DEPENDENT)) {
-    output->concat("\t Battery-backed\n");
-  }
-}
-
-
-/**
-* A constrained invocation function for callbacks following I/O
-*   completion on records.
-*
-* @return 0 on no callback, 1 otherwise.
-*/
-int8_t Storage::_invoke_record_callback(DataRecord* rec, StorageErr err) {
-  int8_t ret = 0;
-  if (nullptr != _cb) {
-    _cb(rec, err);
-    ret++;
-  }
-  return ret;
-}
-
 
 
 /*******************************************************************************
@@ -182,7 +107,7 @@ int8_t DataRecord::save(char* name) {
           uint32_t idx = 0;
           *(rec_desc + idx++) = DATARECORD_SERIALIZER_VERSION;
           *(rec_desc + idx++) = 0;   // No flag fields yet.
-          *(rec_desc + idx++) = _record_type;
+          *(rec_desc + idx++) = (uint8_t) _record_type;
           for (uint8_t i = 0; i < sizeof(_key); i++) {
             *(rec_desc + idx++) = _key[i];
           }
@@ -256,12 +181,14 @@ int8_t DataRecord::load(char* name) {
     _blocks.clear();
     _data_length = 0;
     // To load a record we need a key and a record type.
-    if ((_record_type != 0) & (_key[0] != 0) & (nullptr != _storage)) {
-      ret--;
-      //if (StorageErr::NONE == _storage->persistentRead((DataRecord*) this, _storage->blockSize(), 0)) {
-      //  _dr_set_flag(DATA_RECORD_FLAG_PENDING_IO);
-      //  ret = 0;
-      //}
+    if ((_key[0] != 0) & (nullptr != _storage)) {
+      if ((StorageRecordType::UNINIT != _record_type) & (StorageRecordType::INVALID != _record_type)) {
+        ret--;
+        //if (StorageErr::NONE == _storage->persistentRead((DataRecord*) this, _storage->blockSize(), 0)) {
+        //  _dr_set_flag(DATA_RECORD_FLAG_PENDING_IO);
+        //  ret = 0;
+        //}
+      }
     }
   }
   return ret;
@@ -361,7 +288,7 @@ int8_t DataRecord::buffer_offer_from_storage(uint32_t* addr, uint8_t* buf, uint3
           for (uint32_t i = 0; i < sizeof(_key); i++) {
             key_differs |= (_key[i] == *(buf + DATARECORD_BASE_SIZE + i));
           }
-          if (key_differs | (_record_type != *(buf + 2))) {
+          if (key_differs | (_record_type != (StorageRecordType) *(buf + 2))) {
             // If the key didn't match, instruct the Storage driver to get the
             //   next record block, rather than the next data block.
             for (uint32_t i = 0; i < BLOCK_ADDR_SIZE_BYTES; i++) {
@@ -483,7 +410,7 @@ int8_t DataRecord::_fill_from_descriptor_block(uint8_t* buf) {
 
   _version = DATARECORD_SERIALIZER_VERSION;  // Always migrate old records if possible.
   _flags        = *(buf + 1);
-  _record_type  = *(buf + 2);
+  _record_type  = (StorageRecordType) *(buf + 2);
   for (uint8_t i = 0; i < sizeof(_key); i++) {   *(buf + 3 + i) = _key[i];  }
   _hash = ((uint32_t) *(buf + 12))
     | ((uint32_t) *(buf + 13) << 8)
@@ -506,7 +433,8 @@ int8_t DataRecord::_fill_from_descriptor_block(uint8_t* buf) {
     _nxt_rec_addr = (_nxt_rec_addr << 8) | *(buf + DATARECORD_BASE_SIZE + i);
   }
   // Simple error checking.
-  if (0 != _record_type) {
+
+  if ((StorageRecordType::UNINIT != _record_type) & (StorageRecordType::INVALID != _record_type)) {
     ret--;
     if (_data_length < DEV_BYTES_TOTAL) {
       ret--;
--- src/Storage/Storage.cpp
+++ src/Storage/Storage.cpp
@@ -18,12 +18,84 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-This is the basal implementation of the storage interface.
+This is the basal implementation of the Storage interface.
 */
 
 #include "Storage.h"
+#include "cbor-cpp/cbor.h"
 
+
+const char* Storage::errStr(const StorageErr e) {
+  switch (e) {
+    case StorageErr::KEY_CLOBBERED:    return "KEY_CLOBBERED";
+    case StorageErr::NONE:             return "NONE";
+    case StorageErr::UNSPECIFIED:      return "UNSPECIFIED";
+    case StorageErr::BAD_PARAM:        return "BAD_PARAM";
+    case StorageErr::BUSY:             return "BUSY";
+    case StorageErr::MEM_ALLOC:        return "MEM_ALLOC";
+    case StorageErr::NOT_MOUNTED:      return "NOT_MOUNTED";
+    case StorageErr::NOT_READABLE:     return "NOT_READABLE";
+    case StorageErr::NOT_WRITABLE:     return "NOT_WRITABLE";
+    case StorageErr::NO_FREE_SPACE:    return "NO_FREE_SPACE";
+    case StorageErr::HW_FAULT:         return "HW_FAULT";
+    case StorageErr::KEY_NOT_FOUND:    return "KEY_NOT_FOUND";
+    case StorageErr::KEY_COLLISION:    return "KEY_COLLISION";
+  }
+  return "UNKNOWN";
+}
 
 /*******************************************************************************
-* Statics
+* Storage base-class functions
 *******************************************************************************/
+
+/**
+* Protected constructor
+*/
+Storage::Storage(const uint32_t ds_bytes, const uint32_t bs_bytes) :
+  DEV_SIZE_BYTES(ds_bytes),
+  DEV_BLOCK_SIZE(bs_bytes),
+  DEV_TOTAL_BLOCKS(ds_bytes / bs_bytes),
+  DEV_ADDR_SIZE_BYTES((ds_bytes < 65536) ? 2:4) {};
+
+
+void Storage::_print_storage(StringBuilder* output) {
+  output->concatf("-- Storage [%cr %cw, %sencrypted, %sremovable]\n",
+    _pl_flag(PL_FLAG_MEDIUM_READABLE) ? '+' : '-',
+    _pl_flag(PL_FLAG_MEDIUM_WRITABLE) ? '+' : '-',
+    _pl_flag(PL_FLAG_ENCRYPTED) ? "" : "un",
+    _pl_flag(PL_FLAG_REMOVABLE) ? "" : "non-"
+  );
+  output->concatf("-- %u total bytes across %u pages of %u bytes each.\n", DEV_SIZE_BYTES, DEV_TOTAL_BLOCKS, DEV_BLOCK_SIZE);
+  output->concatf("\t Address size:\t %u\n", DEV_ADDR_SIZE_BYTES);
+  if (isMounted()) {
+    output->concatf("\t Medium mounted (%u bytes free)\t%s\n",
+      freeSpace(),
+      isBusy() ? "[BUSY]" : ""
+    );
+  }
+  if (_pl_flag(PL_FLAG_USES_FILESYSTEM)) {
+    output->concat("\t On top of FS\n");
+  }
+  if (_pl_flag(PL_FLAG_BLOCK_ACCESS)) {
+    output->concat("\t Block access\n");
+  }
+  if (_pl_flag(PL_FLAG_BATTERY_DEPENDENT)) {
+    output->concat("\t Battery-backed\n");
+  }
+}
+
+
+/**
+* A constrained invocation function for callbacks following I/O
+*   completion on records.
+*
+* @return 0 on no callback, 1 otherwise.
+*/
+int8_t Storage::_invoke_record_callback(DataRecord* rec, StorageErr err) {
+  int8_t ret = 0;
+  if (nullptr != _cb) {
+    _cb(rec, err);
+    ret++;
+  }
+  return ret;
+}
